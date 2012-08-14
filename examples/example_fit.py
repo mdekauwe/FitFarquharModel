14b4e36e54c017f08ed8ffac2122cf2f734aff1c
Martin De Kauwe <mdekauwe@IT-4054.local>
2012-08-14 10:50:09 +1000

    cleanup

--- examples/example_fit.py
+++ examples/example_fit.py
@@ -19,7 +19,7 @@ from fit_farquhar_model.farquhar_model import FarquharC3
 from fit_farquhar_model.fit_model import FitJmaxVcmaxRd, FitEaDels
 
 ##############################
-    Fit Jmax, Vcmax + Rd
+# Fit Jmax, Vcmax + Rd
 ##############################
 ofname = "fitting_results.txt"
 ofname25 = "params_at_25.txt"
@@ -32,7 +32,7 @@ F = FitJmaxVcmaxRd(model, ofname, ofname25, results_dir, data_dir, plot_dir)
 F.main(print_to_screen=False)     
 
 ##############################
- Fit Eaj, Eav, delSj + delSv
+# Fit Eaj, Eav, delSj + delSv
 ##############################
 infname = "test_ea_fit.csv"
 ofname = "ea_results.txt"
