ce8d9f2d57b270c3b957d429c4b635d9146330e0
Martin De Kauwe <mdekauwe@IT-4054.local>
2012-08-14 10:42:29 +1000

    complete rewrite of fitting routinues

--- examples/example_fit.py
+++ examples/example_fit.py
@@ -15,16 +15,31 @@ import sys
 import glob
 import numpy as np
 
-from FitFarquharModel import fit_vcmax_jmax_and_rd
-from FitFarquharModel import farquhar_model
+from fit_farquhar_model.farquhar_model import FarquharC3
+from fit_farquhar_model.fit_model import FitJmaxVcmaxRd, FitEaDels
 
+##############################
+    Fit Jmax, Vcmax + Rd
+##############################
 ofname = "fitting_results.txt"
 ofname25 = "params_at_25.txt"
 results_dir = "results"
 data_dir = "data"
 plot_dir = "plots"
-model = farquhar_model.FarquharC3(peaked_Jmax=True, peaked_Vcmax=False)
-
-F = fit_vcmax_jmax_and_rd.FitMe(model, ofname, ofname25, results_dir, data_dir, 
-                                plot_dir)
+model = FarquharC3(peaked_Jmax=True, peaked_Vcmax=True)
+##############################
+F = FitJmaxVcmaxRd(model, ofname, ofname25, results_dir, data_dir, plot_dir)
 F.main(print_to_screen=False)     
+
+##############################
+ Fit Eaj, Eav, delSj + delSv
+##############################
+infname = "test_ea_fit.csv"
+ofname = "ea_results.txt"
+results_dir = "results"
+data_dir = "data"
+model = FarquharC3()
+############################
+F2 = FitEaDels(model, infname, ofname, results_dir, data_dir)
+F2.main(print_to_screen=False)
+
