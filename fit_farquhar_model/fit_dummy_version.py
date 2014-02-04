ff450a60799dfa399d1f7b54e34965a60eae6aa9
Martin De Kauwe <mdekauwe@gmail.com>
2014-02-05 10:33:17 +1100

    cleaned up code

--- fit_farquhar_model/fit_dummy_version.py
+++ fit_farquhar_model/fit_dummy_version.py
@@ -218,19 +218,11 @@ class FitMe(object):
                         max=600.0)
             
             params.add('rdfac_%d' % (leaf_num), value=0.015 , min=0.005, max=0.03) 
-            params.add('Rd25_%d' % (leaf_num), value=Rd25_guess, expr='rdfac_%d * Vcmax25_%d' % (leaf_num, leaf_num))      
+            params.add('Rd25_%d' % (leaf_num), value=Rd25_guess, 
+                       expr='rdfac_%d * Vcmax25_%d' % (leaf_num, leaf_num))      
             
             #params.add('Rd25_%d' % (leaf_num), value=Rd25_guess, min=0.0, 
             #            max=6.0)      
-            
-            
-          
-            
-            
-            
-            #params.add('Rd25_%d' % (leaf_num), value='Vcmax25_guess*0.015', 
-            #            min='Vcmax25_%d*0.005' % (leaf_num), 
-            #            max='Vcmax25_%d*0.03' % (leaf_num), vary=True)
             
             
             # Need to build dummy variable identifier for each leaf.
@@ -284,7 +276,7 @@ class FitMe(object):
         """
         Jmax25 = np.random.uniform(5.0, 550) 
         Vcmax25 = np.random.uniform(5.0, 350) 
-        Rd25 = 0.015 * Vcmax25
+        Rd25 = 0.015 * Vcmax25g
         #Rd25 = np.random.uniform(0.0, 5.0)
         Eaj = np.random.uniform(20000.0, 80000.0)
         Eav = np.random.uniform(20000.0, 80000.0)
