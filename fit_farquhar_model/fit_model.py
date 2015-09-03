ade712b296d6a17a5b4d6a24e799a49ab7d8b746
mdekauwe <mdekauwe@gmail.com>
2015-09-04 08:54:13 +1000

    Commented out code that wasn't being used

--- fit_farquhar_model/fit_model.py
+++ fit_farquhar_model/fit_model.py
@@ -260,10 +260,10 @@ class FitMe(object):
         if data['CO2S'][0] >= 300 and data['CO2S'][0] <= 400:
             row.append("%s" % (data["Photo"][0]))
 
-            if self.elev_correction:
-                ci = data["Ci"][0] * press / 100.
-            else:
-                ci = data["Ci"][0]
+            #if self.elev_correction:
+            #    ci = data["Ci"][0] * press / 100.
+            #else:
+            #    ci = data["Ci"][0]
 
             row.append("%s" % (data["Ci"][0]))
             row.append("%s" % (data["CO2S"][0]))
